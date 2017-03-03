e476f23008058ad4c8d136d872185c9d2c718164
8simsim8 <8simsim8@gmail.com>
2017-03-03 17:01:05 +0200

    Первая страница основная часть

--- js/common.js
+++ js/common.js
@@ -8,3 +8,59 @@ window.addEventListener('scroll', function () {
          header.style.left = - scrollLeft + "px";
     }
 });
+
+
+window.addEventListener('scroll', handlerScrollWindow);
+
+var logoShadow = document.querySelector('.b-slider__shadow');
+
+var ticking = false;
+var scrollPage;
+
+function handlerScrollWindow() {
+    scrollPage =  window.pageYOffset || document.documentElement.scrollTop;
+    if(!ticking) {
+        window.requestAnimationFrame(function() {
+            logoShadowAnim();
+            ticking = false;
+        });
+    }
+    ticking = true;
+}
+
+function logoShadowAnim(){
+    var posEl = logoShadow.getBoundingClientRect();
+
+    if(posEl.top <= window.innerHeight - logoShadow.offsetHeight/2) {
+        logoShadow.classList.remove('shadow');
+    }
+}
+
+/*--------------------------*/
+/*		 Swipe block		*/
+/*--------------------------*/
+// How work
+var swipeSlide,
+    swipeElemClass = '.swiper-container';
+
+swipeSlide = new Swiper(swipeElemClass, {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    setWrapperSize: true
+});
+
+var input = document.getElementsByClassName('input__field');
+
+for(var i = 0; i < input.length; i++) {
+    input[i].addEventListener('focus', function() {
+        this.parentNode.classList.add('input--filled');
+    });
+    input[i].addEventListener('blur', function() {
+        if(this.value == '') {
+            this.parentNode.classList.remove('input--filled');
+        }
+    });
+
+}
