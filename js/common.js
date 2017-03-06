341f2c9155151d3ae6040c5ad6124e570624f088
8simsim8 <8simsim8@gmail.com>
2017-03-06 19:24:28 +0200

    Первая страница

--- js/common.js
+++ js/common.js
@@ -9,10 +9,15 @@ window.addEventListener('scroll', function () {
     }
 });
 
-
 window.addEventListener('scroll', handlerScrollWindow);
 
 var logoShadow = document.querySelector('.b-slider__shadow');
+var paralaxBlock = document.querySelector('.b-parallax');
+var formBlock = document.querySelector('.b-form__bg');
+
+createDataInput();
+
+droplist();
 
 var ticking = false;
 var scrollPage;
@@ -21,18 +26,88 @@ function handlerScrollWindow() {
     scrollPage =  window.pageYOffset || document.documentElement.scrollTop;
     if(!ticking) {
         window.requestAnimationFrame(function() {
-            logoShadowAnim();
+            scrollAnim(logoShadow);
+            scrollAnim(formBlock);
+            paralax(paralaxBlock, scrollPage);
             ticking = false;
         });
     }
     ticking = true;
 }
 
-function logoShadowAnim(){
-    var posEl = logoShadow.getBoundingClientRect();
+function scrollAnim(el) {
+    var posEl = el.getBoundingClientRect();
 
-    if(posEl.top <= window.innerHeight - logoShadow.offsetHeight/2) {
-        logoShadow.classList.remove('shadow');
+    if(posEl.top <= window.innerHeight - el.offsetHeight/2) {
+        el.classList.remove('start');
+    }
+}
+
+function paralax(el, scrollPage) {
+    var posEl = el.getBoundingClientRect();
+
+    var posElToTop = posEl.top + scrollPage;
+
+    if(posEl.top <= window.innerHeight && posEl.bottom > 0) {
+        el.style.backgroundPositionY = (scrollPage - posElToTop)*0.5 + 'px';
+    }
+}
+
+function createDataInput(){
+    var date = document.getElementsByClassName('forcibly-placeholder');
+
+    for(var i = 0, len = date.length; i < len; i++) {
+        date[i].addEventListener('change', function(){
+            if(this.value != '') {
+                this.classList.add('full');
+            } else {
+                this.classList.remove('full');
+            }
+        });
+    }
+}
+
+function droplist() {
+    var droplistsSelect = document.getElementsByClassName('droplist');
+    var droplists = [];
+
+    for(var i = 0, len = droplistsSelect.length; i < len; i++) {
+        var id = '#' + droplistsSelect[i].id;
+        var isDisableOnLoad;
+
+        isDisableOnLoad = (droplistsSelect[i].hasAttribute('data-next-select-id')) ? isDisableOnLoad = true : isDisableOnLoad = false;
+
+        droplists[i] = new Select(id, {
+                // auto show the live filter
+                filtered: 'auto',
+                // auto show the live filter when the options >= 8
+                filter_threshold: 10,
+                // custom placeholder
+                filter_placeholder: 'Filter options...',
+                // on start disable/enable select
+                onLoadDisable: isDisableOnLoad
+            },
+            // callback
+            function (){
+                // close virtual keyboard
+                var filter = this.filter;
+                setTimeout(function(){
+                    filter.blur();
+                },200);
+                // change color of font
+                this.target.parentNode.querySelector('.value').classList.add('select-active');
+
+                // droplist where need to put list by request if need
+                var selector = '[data-next-select-id = ' + this.target.id + ']';
+                var elemBuildSelect = document.querySelector(selector);
+
+                // Test
+                if(elemBuildSelect) {
+                    elemBuildSelect.parentNode.querySelector('.value').removeAttribute('disabled');
+                    // requestResult(this.value, elemBuildSelect);
+                }
+            }
+        );
     }
 }
 
